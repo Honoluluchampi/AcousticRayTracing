7fd2cbe790dba3f367efb330e332d0952366c50d
HonoluluChampi <msd13aj@gmail.com>
2023-05-10 12:13:33 +0900

    impl : closest_hit

--- primitives.hpp
+++ primitives.hpp
@@ -7,9 +7,9 @@ using vec2d = Eigen::Vector2d;
 // Acoustic Ray Tracing
 namespace ART {
 
-static double NO_INTERSECTION = std::numeric_limits<double>::min();
-static uint32_t assign_id = 0;
-using obj_id = uint32_t;
+static double NO_INTERSECTION = std::numeric_limits<double>::max();
+using  obj_id = uint32_t;
+static obj_id assign_id = 0;
 
 struct Sphere
 {
@@ -41,9 +41,32 @@ struct Ray
   double intersection(const Line &line) const;
   double intersection(const Sphere &sphere) const;
 
+  // returns closest hit object and the length from ray.origin
+  template <typename Obj>
+  std::pair<const Obj&, double> closest_hit(const std::vector<Obj>& objs) const;
+
   void reflect(const Line& wall, double length);
 };
 
 std::vector<Ray> create_rays(vec2d origin, int ray_count, int reflection_count);
+
+// impl
+template <typename Obj>
+std::pair<const Obj&, double> Ray::closest_hit(const std::vector<Obj>& objs) const
+{
+  auto closest_length = NO_INTERSECTION;
+  // this will be invalid if closest_length is NO_INTERSECTION at the last of the function
+  int closest_id = 0;
+
+  for (int i = 0; i < objs.size(); i++) {
+    auto new_length = intersection(objs[i]);
+    if (new_length < closest_length) {
+      closest_length = new_length;
+      closest_id = i;
+    }
+  }
+
+  return {objs[closest_id], closest_length};
+}
 
 } // namespace ART
